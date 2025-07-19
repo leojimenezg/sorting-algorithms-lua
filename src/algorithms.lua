157e01a5156e7660b51008f40d028e2ff4a5f1a1
LeoJimenezG <leonardojimenezgz@gmail.com>
2025-07-19 00:55:08 -0600

    feat: implement bogo sort algorithm and basic testing

--- src/algorithms.lua
+++ src/algorithms.lua
@@ -1,9 +1,24 @@
+math.randomseed(os.time())
+
+--[[
+Checks if a string is a valid sorting order.
+@param s: string containing the sorting order.
+@return: Returns "ascending" or "descending". Otherwise, returns nil.
+]]
+local function checkSortOrder(s)
+	if s == "asc" or s == "ascending" then
+		return "ascending"
+	elseif s == "des" or s == "descending" then
+		return "descending"
+	else return nil end
+end
+
 --[[
 Checks if a table in form of an array is sorted in an ascending order.
-If the table is nil, has less than two elements or is ordered, returns true.
+@param t: the table in form of an array to be used by the function.
+@return: returns true if ordered. Otherwise, returns false.
 ]]
 local function checkSortAsc(t)
-	if t == nil or #t < 2 then return true end
 	for i = 1, #t - 1 do
 		if t[i] > t[i + 1] then
 			return false
@@ -13,11 +28,11 @@ local function checkSortAsc(t)
 end
 
 --[[
-Checks if a table in form of an array is sorted in an descending order.
-If the table is nil, has less than two elements or is ordered, returns true.
+Checks if a table in form of an array is sorted in a descending order.
+@param t: the table in form of an array to be used by the function.
+@return: returns true if ordered. Otherwise, returns false.
 ]]
 local function checkSortDes(t)
-	if t == nil or #t < 2 then return true end
 	for i = 1, #t - 1 do
 		if t[i] < t[i + 1] then
 			return false
@@ -26,24 +41,45 @@ local function checkSortDes(t)
 	return true
 end
 
+--[[
+Shuffles all the elements of a table using Fisher-Yates algorithm.
+@param t: the table in form of an array to be used by the function.
+@return: returns the shuffled table.
+Warning: the sorting process happens in place, modifying the original table.
+]]
+local function shuffleTable(t)
+	for i = #t, 1, -1 do
+		local random_idx = math.random(1, i)
+		local temp = t[i]
+		t[i] = t[random_idx]
+		t[random_idx] = temp
+	end
+	return t
+end
+
 Algorithms = {}
 
 --[[
 BogoSort shuffles the table randomly until it is ordered.
 @param t: table in form of an array to be used by the algorithm.
-@param inplace: boolean that indicates if the sorting is made inplace or in a new table (default: false).
-@return: returns nil if t is nil, or if inplace is true (sorting made in same table). Otherwise, returns the sorted table.
+@param order: string that indicates the order of sorting (default: "ascending").
+@return: returns the sorted table. Otherwise, returns nil if t is nil.
+Warning: the sorting process happens in place, modifying the original table.
 ]]
-function Algorithms.bogoSort(t, inplace)
+function Algorithms.bogoSort(t, order)
 	if t == nil then return nil end
-	inplace = inplace or false
-	if inplace == false then
-		if #t < 2 then return t end
-		--Actual implementation.
-	elseif inplace == true then
-		if #t < 2 then return nil end
-		--Actual implementation.
+	if #t < 2 then return t end
+	order = checkSortOrder(order) or "ascending"
+	local checkSort
+	if order == "ascending" then
+		checkSort = checkSortAsc
+	else
+		checkSort = checkSortDes
 	end
+	while not checkSort(t) do
+		t = shuffleTable(t)
+	end
+	return t
 end
 
 function Algorithms.selectionSort(t, inplace)
@@ -95,7 +131,7 @@ function Algorithms.quickSort(t, inplace)
 end
 
 function Algorithms.isSorted(t, order)
-	order = order or "asc"
+	order = order or "ascending"
 	if order == "asc" or order == "ascending" then
 		return checkSortAsc(t)
 	elseif order == "des" or order == "descending" then
--- src/main.lua
+++ src/main.lua
@@ -2,3 +2,27 @@ require("src.algorithms")
 
 local algorithms = Algorithms
 
+local tests = {
+	--Basic cases.
+	{3, 1, 4, 1, 5, 9, 2, 6},
+	{5, 4, 3, 2, 1},
+	{1, 2, 3, 4, 5},
+	--Extreme cases.
+	{42},
+	{},
+	{7, 7, 7, 7},
+	{100, -50, 0, 25, -100},
+	--Worst case.
+	{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
+}
+
+local order = "asc"
+local algorithm = algorithms.bogoSort
+for i, test in pairs(tests) do
+	print("Test " .. i)
+	test = algorithm(test, order)
+	if test == nil then break end
+	for j=1, #test do
+		print(test[j])
+	end
+end
